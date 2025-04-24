f078cf205ccbed4eb74b0745726cdfedc78187c8
Matthew Kwok <mrkwok@usc.edu>
2025-04-24 16:18:55 -0700

    First Submission

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,9 +21,49 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool helper(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<int>& shiftCounts,
+    size_t day,
+    size_t slot);
 
 // Add your implementation of schedule() and other helper functions here
+
+bool helper(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<int>& shiftCounts,
+    size_t day,
+    size_t slot)
+{
+    size_t n = avail.size();
+    size_t k = avail[0].size();
+
+    if (day == n) { //base case is if all days are filled
+        return true;
+    }
+
+    if (slot == dailyNeed) { //if current day is scheduled, move on
+        return helper(avail, dailyNeed, maxShifts, sched, shiftCounts, day + 1, 0);
+    }
+
+    for (size_t worker = 0; worker < k; worker++) { //try each worker
+        if (avail[day][worker] && shiftCounts[worker] < static_cast<int>(maxShifts) && find(sched[day].begin(), sched[day].end(), worker) == sched[day].end()) { //conditions are if worker is available/worker has not gone past maxShifts/worker is not already scheduled
+            sched[day].push_back(worker); //assign worker
+            shiftCounts[worker]++;
+
+            if (helper(avail, dailyNeed, maxShifts, sched, shiftCounts, day, slot + 1)) { //recursion to next slot
+                return true;
+            }
+            sched[day].pop_back(); //backtrack
+            shiftCounts[worker]--;
+        }
+    }
+    return false;
+}
 
 bool schedule(
     const AvailabilityMatrix& avail,
@@ -37,9 +77,13 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    size_t n = avail.size(); //num days
+    size_t k = avail[0].size(); //num workers
 
+    sched.resize(n); //scheudle has n days
+    vector<int> shiftCounts(k,0); //shift for each worker
 
-
+    return helper(avail, dailyNeed, maxShifts, sched, shiftCounts, 0, 0); //recursive backtrack from day 0/slot 0
 
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void makeWords(std::string currWord,std::string floating, const std::set<std::string>& dict, std::set<std::string>& res, size_t ind);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +22,55 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    std::set<std::string> res;
+    makeWords(in, floating, dict, res, 0); //recursion starts at idx 0
+    return res;
 }
 
 // Define any helper functions here
+
+void makeWords(std::string currWord, std::string floating, const std::set<std::string>& dict, std::set<std::string>& res, size_t ind)
+{
+    if (ind == currWord.size()) { //base case is if reached the end of the word
+        if (floating.empty() && dict.find(currWord) != dict.end()) {
+            res.insert(currWord);
+        }
+        return;
+    }
+
+    if (currWord[ind] != '-') { //if curr pos is filled, move on
+        makeWords(currWord, floating, dict, res, ind + 1);
+        return;
+    }
+    /*
+    for (char ch = 'a'; ch <= 'z'; ch++) {
+        currWord[ind] = ch; */
+
+    size_t remainingDashes = 0; //if remaining blanks < floating letters, can't place all of them
+    for (size_t i = ind; i < currWord.size(); ++i) {
+        if (currWord[i] == '-') remainingDashes++;
+    }
+    if (remainingDashes < floating.size()) return;
+
+    //try placing unused floating chars at pos
+    std::set<char> tried;
+    for (size_t i = 0; i < floating.size(); ++i) {
+        char ch = floating[i];
+        if (tried.count(ch)) continue; //skips repeated chars
+        tried.insert(ch);
+
+        currWord[ind] = ch;
+        std::string newFloating = floating;
+        newFloating.erase(newFloating.find(ch), 1); //remove used floating char
+        makeWords(currWord, newFloating, dict, res, ind + 1); //recurse with new word
+    }
+
+   
+    for (char ch = 'a'; ch <= 'z'; ++ch) { //try all other chars after recursing
+        if (floating.find(ch) != std::string::npos) continue; //skip the floating letters
+        currWord[ind] = ch;
+        makeWords(currWord, floating, dict, res, ind + 1);
+    }
+
+    currWord[ind] = '-';
+}
